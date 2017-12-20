09d45659f24da9b5f3d67b612e97c220876e6d8a
Bojana <bojana.karakacev@gmail.com>
2017-12-20 13:51:39 +0100

    Add functions for the game.

--- src/application.js
+++ src/application.js
@@ -1,1 +1,58 @@
 // Application code goes here
+
+function playRound(playerSelection, computerSelection) {
+
+  if(playerSelection === computerSelection) {
+    console.log('The result is a tie! Next round.');
+  }
+  if(playerSelection === 'rock') {
+    if(computerSelection === 'paper') {
+      console.log('Computer wins! Paper beats rock.');
+    } else {
+        if(computerSelection === 'scissors') {
+          console.log('Player wins! Rock beats scissors.');
+      }
+    }
+  }
+  if(playerSelection === 'paper') {
+    if(computerSelection === 'rock') {
+      console.log('You win! Paper beats rock.');
+    } else {
+        if(computerSelection === 'scissors') {
+          console.log('Computer wins! Scissors beats paper.'); 
+      }
+    }
+  }
+  if(playerSelection === 'scissors') {
+    if(computerSelection === 'paper') {
+      console.log('You win! Scissors beats paper.');
+    } else { 
+        if(computerSelection === 'rock') {
+          console.log('Computer wins! Rock beats scissors.');
+      }
+    }
+  }
+}
+
+function game() {
+  for(let i = 0; i < 5; i ++) {
+
+    let playerChoice = prompt('What is your choice: rock, paper or scissors?');
+    let computerChoice = Math.random();
+
+    if(computerChoice <= 0.33) {
+      computerChoice = 'rock';
+    } else if(computerChoice <= 0.67) {
+      computerChoice = 'paper';
+    } else {
+      computerChoice = 'scissors';  
+    }
+
+    console.log(`Player choice is: ${playerChoice}`);
+    console.log(`Computer choice is: ${computerChoice}`);
+    playRound(playerChoice, computerChoice);
+  }
+}
+
+game();
+ 
