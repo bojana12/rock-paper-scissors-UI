61dcb05fd5cd8f680babc351a3ccbb6463df9aa2
Bojana <bojana.karakacev@gmail.com>
2018-01-12 21:30:34 +0100

    Add buttons and modify functions

--- src/application.js
+++ src/application.js
@@ -1,92 +1,78 @@
-import $ from 'jquery';
-import swal from 'sweetalert2';
-
-$(document).ready(function(){
-  $('button').on('click', function(){
-    game();
-  });
-});
-
 let playerScore = 0;
 let computerScore = 0;
 
-function playRound(playerSelection, computerSelection) {
+const results = document.querySelector('.results');
+const choices = document.querySelector('.choices');
 
-  if(playerSelection.toLowerCase() === computerSelection) {
-    console.log('The result is a tie!');
+
+function playRound(playerSelection) {
+
+  let computerSelection = Math.random();
+
+  if(computerSelection <= 0.33) {
+    computerSelection = 'rock';
+  } else if(computerSelection <= 0.67) {
+    computerSelection = 'paper';
+  } else {
+    computerSelection = 'scissors';  
+  }
+
+  if(playerSelection === computerSelection) {
+    choices.textContent = 'The result is a tie!';
     return;
   }
 
-  if(playerSelection.toLowerCase() === 'rock') {
+  if(playerSelection === 'rock') {
     if(computerSelection === 'paper') {
-      console.log('Computer wins! Paper beats rock.');
+      choices.textContent = `You chose ${playerSelection} and Computer chose ${computerSelection}. Paper beats rock.`;
       computerScore += 1;
     } else {
-      console.log('You win! Rock beats scissors.');
+      choices.textContent = `You chose ${playerSelection} and Computer chose ${computerSelection}. Rock beats scissors.`;
       playerScore += 1; 
     }
   }
 
-  if(playerSelection.toLowerCase() === 'paper') {
+  if(playerSelection === 'paper') {
     if(computerSelection === 'rock') {
-      console.log('You win! Paper beats rock.');
+      choices.textContent = `You chose ${playerSelection} and Computer chose ${computerSelection}. Paper beats rock.`;
       playerScore += 1;
     } else {
-      console.log('Computer wins! Scissors beats paper.');
+      choices.textContent = `You chose ${playerSelection} and Computer ${computerSelection}. Scissors beats paper.`;
       computerScore += 1;
     }
   }
 
-  if(playerSelection.toLowerCase() === 'scissors') {
+  if(playerSelection === 'scissors') {
     if(computerSelection === 'paper') {
-      console.log('You win! Scissors beats paper.');
+      choices.textContent = `You chose ${playerSelection} and Computer chose ${computerSelection}. Scissors beats paper.`;
       playerScore += 1;
     } else { 
-      console.log('Computer wins! Rock beats scissors.');
+      choices.textContent = `You chose ${playerSelection} and Computer chose ${computerSelection}. Computer wins! Rock beats scissors.`;
       computerScore += 1;    
     }
   }
+
+  scores(playerScore, computerScore);
+
+  return;
 }
 
-function game() {
-  for(let i = 0; i < 5; i ++) {
-    let playerChoice = prompt('What is your choice: rock, paper or scissors?'); 
-    let computerChoice = Math.random();
 
-    if(!validInput(playerChoice.toLowerCase())) {
-      swal('Enter rock, paper or scissors!');
-      return;
-    } 
-
-    if(computerChoice <= 0.33) {
-      computerChoice = 'rock';
-    } else if(computerChoice <= 0.67) {
-      computerChoice = 'paper';
-    } else {
-      computerChoice = 'scissors';  
+function scores(playerScore, computerScore) {
+    results.textContent = `You have ${playerScore} and computer has ${computerScore}.`;
+    if(playerScore === 5) {
+      results.textContent = `Congratulations you win! You have 5 points.`;
+      document.location.reload(true);
+    } else if(computerScore === 5) {
+      results.textContent = `Computer wins! Sorry, more luck next time!`;
+      document.location.reload(true);
     }
+} 
 
-    console.log(`Player choice is: ${playerChoice}`);
-    console.log(`Computer choice is: ${computerChoice}`);
-    playRound(playerChoice, computerChoice);
-  }
+const buttons = document.querySelectorAll('button');
 
-  winner(playerScore, computerScore);
-}
-
-function validInput(playerChoice) {
-  const choices = ['rock', 'paper', 'scissors'];
-  return choices.includes(playerChoice);
-}
-
-function winner(playerScore, computerScore) {
-  if(playerScore > computerScore) {
-    console.log(`You have ${playerScore} and computer has ${computerScore}. Congratulations! You are the winner`)
-  } else if(playerScore < computerScore) {
-    console.log(`Sorry, you lost. You have ${playerScore} and computer has ${computerScore}. More luck next time!`);
-  } else {
-    console.log(`The result is a tie! You have ${playerScore} and computer has ${computerScore}.`);
-  }
-}
-
-
+buttons.forEach((button) => {
+  button.addEventListener('click', function(e) {
+    playRound(e.target.dataset.id);
+  });
+});
