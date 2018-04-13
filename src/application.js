76758dd0829ed7d055d699bdf5acb0eccd6304bb
Bojana <bojana.karakacev@gmail.com>
2018-04-13 19:37:46 +0200

    Change text

--- src/application.js
+++ src/application.js
@@ -17,38 +17,37 @@ function playRound(playerSelection) {
     computerSelection = 'scissors';  
   }
 
-  
   if(playerSelection === computerSelection) {
-    choices.textContent = 'The result is a tie!';
+    choices.textContent = 'Your choice and computer choice is the same!';
     return;
   }
 
   if(playerSelection === 'rock') {
     if(computerSelection === 'paper') {
-      choices.textContent = `You chose ${playerSelection} and Computer chose ${computerSelection}. Paper beats rock.`;
+      choices.textContent = `You chose ${playerSelection} and computer chose ${computerSelection}. Paper beats rock.`;
       computerScore += 1;
     } else {
-      choices.textContent = `You chose ${playerSelection} and Computer chose ${computerSelection}. Rock beats scissors.`;
+      choices.textContent = `You chose ${playerSelection} and computer chose ${computerSelection}. Rock beats scissors.`;
       playerScore += 1; 
     }
   }
 
   if(playerSelection === 'paper') {
     if(computerSelection === 'rock') {
-      choices.textContent = `You chose ${playerSelection} and Computer chose ${computerSelection}. Paper beats rock.`;
+      choices.textContent = `You chose ${playerSelection} and computer chose ${computerSelection}. Paper beats rock.`;
       playerScore += 1;
     } else {
-      choices.textContent = `You chose ${playerSelection} and Computer ${computerSelection}. Scissors beats paper.`;
+      choices.textContent = `You chose ${playerSelection} and computer chose ${computerSelection}. Scissors beats paper.`;
       computerScore += 1;
     }
   }
 
   if(playerSelection === 'scissors') {
     if(computerSelection === 'paper') {
-      choices.textContent = `You chose ${playerSelection} and Computer chose ${computerSelection}. Scissors beats paper.`;
+      choices.textContent = `You chose ${playerSelection} and computer chose ${computerSelection}. Scissors beats paper.`;
       playerScore += 1;
     } else { 
-      choices.textContent = `You chose ${playerSelection} and Computer chose ${computerSelection}. Rock beats scissors.`;
+      choices.textContent = `You chose ${playerSelection} and computer chose ${computerSelection}. Rock beats scissors.`;
       computerScore += 1;    
     }
   }
@@ -60,7 +59,7 @@ function playRound(playerSelection) {
 function scores(playerScore, computerScore) {
   results.textContent = `You have ${playerScore} and computer has ${computerScore}.`;
   if(playerScore === 5) {
-    results.textContent = `Congratulations you win! You have 5 points. New game will begin in 10 seconds.`;
+    results.textContent = `Congratulations, you win! You have 5 points. New game will begin in 10 seconds.`;
     timeout(10000);
   } else if(computerScore === 5) {
     results.textContent = `Computer has 5 points, computer wins! Sorry, more luck next time! New game will begin in 10 seconds.`;
